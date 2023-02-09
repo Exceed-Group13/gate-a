405703653008973d542e199c6da04cfd0487c9b8
Beambyp <pariyakorn.kam@ku.th>
2023-02-09 17:39:01 +0700

    ChangeBranchName

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
+import Profile from './pages/ResetPass';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
---
+++ src/pages/ResetPass.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/Register.css'
+
+const Register = () => {
+  return (
+    <div className="register">
+      <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'/>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+      <div class="component">
+        <h1>Reset Password</h1>
+        <br/>
+
+        <br/>
+        <form class="form" method="post" >
+          <input className='Old-Pass' placeholder="Old password"/>
+          <input className='New-Pass' placeholder="New Password"/>
+        <br/>
+        <br/>
+          <input type="submit" class="submit" value="Change Password"/>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
