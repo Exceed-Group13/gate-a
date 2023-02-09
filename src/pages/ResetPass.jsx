d961c4a6171320743b75f46d7b2d9be9141e4709
Beambyp <pariyakorn.kam@ku.th>
2023-02-09 18:05:29 +0700

    fix input fill

--- src/pages/ResetPass.jsx
+++ src/pages/ResetPass.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import '../styles/ResetPass.css'
+import '../styles/Register.css'
 
 const Register = () => {
   return (
-    <div className="reset">
+    <div className="register">
       <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'/>
       <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
-      <div class="component">
+      <div class="wrapper">
         <h1>Reset Password</h1>
         <br/>
 
         <br/>
         <form class="form" method="post" >
-          <input className='Old-Pass' placeholder="Old password"/>
-          <input className='New-Pass' placeholder="New Password"/>
+          <input className='input-name' placeholder="Old password"/>
+          <input className='input-pass' placeholder="New Password"/>
         <br/>
         <br/>
           <input type="submit" class="submit" value="Change Password"/>
