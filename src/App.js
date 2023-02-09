8122addbf6822d1f1e6193074a55cedf4f168897
Fortran <kawintida.pi@ku.th>
2023-02-09 18:53:53 +0700

    add popup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
+import Changepass from './pages/Changepass';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Reset />} /> */}
+        <Route path="/changepass" element={<Changepass />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
