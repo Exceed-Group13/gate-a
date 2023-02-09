508cb4a011a18e240e6344a9e9be15d52883844b
natekrth <natekrth@gmail.com>
2023-02-09 16:04:28 +0700

    add menu components

--- src/pages/Control.jsx
+++ src/pages/Control.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import Menu from "../components/Menu";
+import Button from 'react-bootstrap/Button';
+import '../styles/Control.css'
 
 const Control = () => {
-  return (
-    <div>Control</div>
-  )
-}
 
-export default Control
+  return (
+    <>
+        <div>
+            <Menu menu1={"Manage"} menu2={"Password"} />
+        </div>
+        <div>
+            <Button className="but" variant="outline-success">Success</Button>{' '}
+            <Button variant="outline-danger">Danger</Button>{' '}
+        </div>
+    </>
+  );
+};
+
+export default Control;
