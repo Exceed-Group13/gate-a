fb3e2eb4fcc1afc82e502fd20bc302d98c8349b4
Beambyp <pariyakorn.kam@ku.th>
2023-02-09 15:41:53 +0700

    register

---
+++ src/pages/Register.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/Register.css'
+
+const Register = () => {
+  return (
+    <div className="register">
+      <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'/>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+      <div class="wrapper">
+        <h1>Register Key</h1>
+        <br/>
+
+        <br/>
+        <form class="form" method="post" action="yourpage.html">
+          <input type="text" class="name" placeholder="Key Name"/>
+          <input type="email" class="email" placeholder="Key Password"/>
+        <br/>
+        <br/>
+          <input type="submit" class="submit" value="Register"/>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
