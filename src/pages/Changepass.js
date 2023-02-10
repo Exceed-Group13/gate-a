43f4e625b230a85460c4ec1197bcc0eecd2b4fc2
natekrth <natekrth@gmail.com>
2023-02-11 01:14:02 +0700

    check oldpassword from backend with input

--- src/pages/Changepass.js
+++ src/pages/Changepass.js
@@ -1,4 +1,4 @@
-import React, { useState , useRef } from 'react'
+import React, { useState , useEffect, useRef } from 'react'
 
 import Swal from 'sweetalert2'
 import '../styles/Register.css'
@@ -7,10 +7,20 @@ const Changepass = () => {
   const [oldPassword, setOldPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [data, setData] = useState(undefined)
+  const URL = "https://ecourse.cpe.ku.ac.th/exceed13/home"
 
   const oldPasswordEl = useRef()
   const newPasswordEl = useRef()
   const confirmPasswordEl = useRef()
+
+  function checkOldPassword(oldPassword,e){
+    if ((e.target.value).length == 0 || e.target.value.charCodeAt((e.target.value).length-1) === 49 || e.target.value.charCodeAt((e.target.value).length-1) === 50 || e.target.value.charCodeAt((e.target.value).length-1) === 51) {
+        setOldPassword(e.target.value)
+    } else {
+      setOldPassword(oldPassword)
+    }
+  }
 
   function checkNewPassword(newPassword,e){
     if ((e.target.value).length == 0 || e.target.value.charCodeAt((e.target.value).length-1) === 49 || e.target.value.charCodeAt((e.target.value).length-1) === 50 || e.target.value.charCodeAt((e.target.value).length-1) === 51) {
@@ -28,12 +38,20 @@ const Changepass = () => {
     }
   }
 
+  useEffect(()=>{
+    fetch(URL).then((response) => response.json()).then((response) => {
+      setData(response.result); 
+      console.log(response)
+      console.log(data)
+    })
+  })
+
   function manageKey(old, newpin) {
     const requestOptions = {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        house_name: "house",
+        house_name: data[0]['house_name'],
         old_pin: [...old].map(str => {return parseInt(str, 10)}),
         new_pin: [...newpin].map(str => {return parseInt(str, 10)}),
       }),
@@ -61,14 +79,23 @@ const Changepass = () => {
         title: 'Oops...',
         text: 'Your new password cannot be the same as old password',
       })
-    } else {
+    } 
+    else {
       Swal.fire({
         icon: 'success',
         title: 'Password Changed',
         text: 'Your password has been successfully changed',
       })
     }
-    manageKey(oldPasswordEl.current.value, newPasswordEl.current.value)
+    if (oldPasswordEl.current.value == Number(data[0]['pin'].join(''))) {
+      manageKey(oldPasswordEl.current.value, newPasswordEl.current.value)
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Incorrect Old Password',
+        text: 'Please corrected your old password',
+      })
+    }
   }
 
   return (
@@ -83,24 +110,30 @@ const Changepass = () => {
         <form class="form" method="post" onSubmit={(ev) => handleSubmit(ev)}>
           <input 
             className='input-name' 
+            // type="password"
             placeholder="Old password"
             value={oldPassword}
-            onChange={(e) => setOldPassword(e.target.value)}
+            onChange={(e) => checkOldPassword(oldPassword, e)}
             ref={oldPasswordEl}
+            maxLength={3}
           />
           <input 
             className='input-pass' 
+            // type="password"
             placeholder="New Password"
             value={newPassword}
             onChange={(e) => checkNewPassword(newPassword,e)}
             ref={newPasswordEl}
+            maxLength={3}
           />
           <input 
             className='input-confirm' 
+            // type="password"
             placeholder="Confirm Password"
             value={confirmPassword}
             onChange={(e) => checkConfirmPassword(confirmPassword, e)}
             ref={confirmPasswordEl}
+            maxLength={3}
           />
         <br/>
         <br/>
