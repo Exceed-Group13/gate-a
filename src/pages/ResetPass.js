d6c02a56a4cb32c775c994bcef71bede5785c394
Fortran <kawintida.pi@ku.th>
2023-02-10 16:46:53 +0700

    add menu bar

--- src/pages/ResetPass.js
+++ src/pages/ResetPass.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../styles/ResetPass.css'
+import Menu from "../components/Menu";
 
 const Register = () => {
   return (
+    
     <div className="register">
+      <div>
+            <Menu menu1={"Manage"} menu2={"Password"} />
+      </div>
       <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'/>
       <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
       <div className='wrapper'>
