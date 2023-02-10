09b02dd47fa1b3c301c11a8a2d44c14530a02089
natekrth <natekrth@gmail.com>
2023-02-10 18:25:24 +0700

    resetpasword sucs to backedng

--- src/pages/Changepass.js
+++ src/pages/Changepass.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState , useRef } from 'react'
+
 import Swal from 'sweetalert2'
 import '../styles/Register.css'
 
@@ -6,6 +7,10 @@ const Changepass = () => {
   const [oldPassword, setOldPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+
+  const oldPasswordEl = useRef()
+  const newPasswordEl = useRef()
+  const confirmPasswordEl = useRef()
 
   function checkNewPassword(newPassword,e){
     if ((e.target.value).length == 0 || e.target.value.charCodeAt((e.target.value).length-1) === 49 || e.target.value.charCodeAt((e.target.value).length-1) === 50 || e.target.value.charCodeAt((e.target.value).length-1) === 51) {
@@ -23,17 +28,34 @@ const Changepass = () => {
     }
   }
 
+  function manageKey(old, newpin) {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        house_name: "house",
+        old_pin: [...old].map(str => {return parseInt(str, 10)}),
+        new_pin: [...newpin].map(str => {return parseInt(str, 10)}),
+      }),
+    };
+
+    fetch("https://ecourse.cpe.ku.ac.th/exceed13/resetpin", requestOptions)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err))
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if (!oldPassword || !newPassword || !confirmPassword) {
+    if (!oldPasswordEl.current.value || !newPasswordEl.current.value || !confirmPasswordEl.current.value) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: 'Please enter both the old password and the new password',
       })
     }
-    else if (oldPassword == newPassword) {
+    else if (oldPasswordEl.current.value == newPasswordEl.current.value) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -46,6 +68,7 @@ const Changepass = () => {
         text: 'Your password has been successfully changed',
       })
     }
+    manageKey(oldPasswordEl.current.value, newPasswordEl.current.value)
   }
 
   return (
@@ -57,24 +80,27 @@ const Changepass = () => {
         <br/>
 
         <br/>
-        <form class="form" method="post" onSubmit={handleSubmit}>
+        <form class="form" method="post" onSubmit={(ev) => handleSubmit(ev)}>
           <input 
             className='input-name' 
             placeholder="Old password"
             value={oldPassword}
             onChange={(e) => setOldPassword(e.target.value)}
+            ref={oldPasswordEl}
           />
           <input 
             className='input-pass' 
             placeholder="New Password"
             value={newPassword}
             onChange={(e) => checkNewPassword(newPassword,e)}
+            ref={newPasswordEl}
           />
           <input 
             className='input-confirm' 
             placeholder="Confirm Password"
             value={confirmPassword}
             onChange={(e) => checkConfirmPassword(confirmPassword, e)}
+            ref={confirmPasswordEl}
           />
         <br/>
         <br/>
